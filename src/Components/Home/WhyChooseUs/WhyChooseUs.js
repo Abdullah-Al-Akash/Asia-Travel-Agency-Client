e5fd7c237700e20e54160f05c7300bc8577ada4e
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-31 15:55:59 +0600

    Update Extra section

--- src/Components/Home/WhyChooseUs/WhyChooseUs.js
+++ src/Components/Home/WhyChooseUs/WhyChooseUs.js
@@ -11,25 +11,26 @@ const WhyChooseUs = () => {
                                 <h1 className="brand-color fw-bold">Why Choose Us</h1>
                                 <h6>Here are reasons you should plan trip with us</h6>
                                 <div className="row text-center mt-5 pt-3 pb-5">
-                                        <div className="col-lg-4">
+                                        <div className="col-lg-4 p-2">
                                                 <img src={img1} className="img-fluid" width="120" alt="" />
                                                 <div className="mt-3">
                                                         <h4 className="brand-color fw-bold">Handpicked Hotels</h4>
-                                                        <p>Julia's love of historical buildings, the English countryside, local food, and good wine inspired her dream to own a collection of hotels that would</p>
+                                                        <p className="mt-3">Julia's love of historical buildings, the English countryside, local food, and good wine inspired her dream to own a collection of hotels that would</p>
                                                 </div>
                                         </div>
-                                        <div className="col-lg-4">
+                                        <div className="col-lg-4 p-2">
                                                 <img src={img2} className="img-fluid" width="120" alt="" />
                                                 <div className="mt-3">
-                                                        <h4 className="brand-color fw-bold">Handpicked Hotels</h4>
-                                                        <p>Julia's love of historical buildings, the English countryside, local food, and good wine inspired her dream to own a collection of hotels that would</p>
+                                                        <h4 className="brand-color fw-bold">World Class Service</h4>
+                                                        <p className="mt-3">World class service is defined as vastly exceeding customers' expectations and building brand loyalty as a result.</p>
                                                 </div>
                                         </div>
-                                        <div className="col-lg-4">
+                                        <div className="col-lg-4 p-2">
                                                 <img src={img3} className="img-fluid" width="120" alt="" />
                                                 <div className="mt-3">
-                                                        <h4 className="brand-color fw-bold">Handpicked Hotels</h4>
-                                                        <p>Julia's love of historical buildings, the English countryside, local food, and good wine inspired her dream to own a collection of hotels that would</p>
+                                                        <h4 className="brand-color fw-bold">Best Price Guarantee</h4>
+                                                        <p className="mt-3">Our Best Price Guarantee means that you can be sure of booking at the best rate.
+                                                                If you find a lower price elsewhere within 24 hours.</p>
                                                 </div>
                                         </div>
                                 </div>
